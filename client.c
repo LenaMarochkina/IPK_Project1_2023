b721f554b8ec8978f56bf63e7d2551af15b0240a
Lena <xmaroc00@stud.fit.vutbr.cz>
2023-03-18 22:06:47 +0100

    fix: fixes UDP function for constant messaging from user

--- client.c
+++ client.c
@@ -6,9 +6,10 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
-
+#include <signal.h>
 
 #define BUF_SIZE 1024
+
 
 //Function to send message to server and receive answer using TCP
 void tcp_client(const char* server_ip, int server_port) {
@@ -66,7 +67,6 @@ void tcp_client(const char* server_ip, int server_port) {
 
 void udp_client(const char* server_ip, int server_port) {
     // Create a UDP socket
-    // TODO: Change bite shift
     int sock = socket(AF_INET, SOCK_DGRAM, 0);
     if (sock < 0) {
         perror("Failed to create socket");
@@ -81,31 +81,34 @@ void udp_client(const char* server_ip, int server_port) {
     server_addr.sin_port = htons(server_port);
 
     // Prompt the user for a message
-    char message[BUF_SIZE];
-    printf("Enter a message: ");
-    fgets(message, BUF_SIZE, stdin);
-    printf("Message: %d\n", strlen(message));
+    char buffer[BUF_SIZE];
+    while (strcmp(buffer, "BYE\n") != 0) {
+        bzero (buffer, BUF_SIZE);
+        fgets(buffer, BUF_SIZE, stdin);
+        //add \0 to the start of buffer
+        char new_buffer[] = "\0";
+        strcat(new_buffer, buffer);
+        printf("Sending message: %s\n", new_buffer);
+        // Send the message to the server
+        int len = sendto(sock, new_buffer, strlen(buffer), 0, (struct sockaddr *) &server_addr, sizeof(server_addr));
+        if (len < 0) {
+            perror("Failed to send message");
+            close(sock);
+            exit(1);
+        }
 
-    // Send the message to the server
-    int len = sendto(sock, message, strlen(message), 0, (struct sockaddr *) &server_addr, sizeof(server_addr));
-    if (len < 0) {
-        perror("Failed to send message");
-        close(sock);
-        exit(1);
+        // Receive the server's response
+        char buffer[BUF_SIZE];
+        memset(buffer, 0, BUF_SIZE);
+        len = recvfrom(sock, buffer, BUF_SIZE, 0, NULL, NULL);
+        if (len < 0) {
+            perror("Failed to receive response");
+            close(sock);
+            exit(1);
+        }
+
+        printf("Received response: %s\n", buffer);
     }
-
-    // Receive the server's response
-    char buffer[1024];
-    memset(buffer, 0, BUF_SIZE);
-    len = recvfrom(sock, buffer, BUF_SIZE, 0, NULL, NULL);
-    if (len < 0) {
-        perror("Failed to receive response");
-        close(sock);
-        exit(1);
-    }
-
-    printf("Received response: %s\n", buffer);
-
     close(sock);
 }
 
@@ -139,7 +142,7 @@ int main(int argc, char *argv[]) {
     }
     printf("host=%s, port=%d, mode=%s\n", host, port, mode);
     if (strcmp(mode, "tcp") == 0){
-        //tcp_client(host, port);
+        tcp_client(host, port);
     }
     else if (strcmp(mode, "udp") == 0){
         printf("here\n");
