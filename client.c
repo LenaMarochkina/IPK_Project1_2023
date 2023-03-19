6cb71effcdfbe76723e7a3824f95555324bcf59c
Lena <xmaroc00@stud.fit.vutbr.cz>
2023-03-19 15:11:31 +0100

    feat: adds global variables, validation,  creates separate function for socket creation

--- client.c
+++ client.c
@@ -10,18 +10,63 @@
 
 #define BUF_SIZE 1024
 
-//Function to send message to server and receive answer using TCP
-void tcp_client(const char* server_ip, int server_port) {
-    int sockfd;
-    struct sockaddr_in server_addr;
-    char buffer[BUF_SIZE];
+//Global variables
+char buffer[BUF_SIZE];
+int SOCKET = -1;
 
-    // Create a socket
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0) {
-        perror("socket");
+//Function to validate arguments
+void validate(char* host, int port, char* mode){
+
+    //check if all arguments are entered
+    if (host == NULL || port == 0 || mode == NULL) {
+        fprintf(stderr, "Usage: %s -h host -p port -m mode\n");
         exit(EXIT_FAILURE);
     }
+
+    //check if port is valid
+    if (port < 1024 || port > 65535) {
+        fprintf(stderr, "Invalid port number. Please enter a port number between 1024 and 65535\n");
+        exit(EXIT_FAILURE);
+    }
+
+    //check if mode is valid
+    if (strcmp(mode, "tcp") != 0 && strcmp(mode, "udp") != 0) {
+        fprintf(stderr, "Invalid mode. Please enter tcp or udp\n");
+        exit(EXIT_FAILURE);
+    }
+
+    //check if host is valid
+    struct sockaddr_in sa;
+    int result = inet_pton(AF_INET, host, &(sa.sin_addr));
+    if (result == 0) {
+        fprintf(stderr, "Invalid host. Please enter a valid host\n");
+        exit(EXIT_FAILURE);
+    }
+}
+
+//Function to create socket
+void create_socket (char* mode) {
+
+    if (strcmp(mode, "tcp") == 0) {
+        SOCKET = socket(AF_INET, SOCK_STREAM, 0);
+        if (SOCKET < 0) {
+            perror("socket");
+            exit(EXIT_FAILURE);
+        }
+
+    }
+    else if (strcmp(mode, "udp") == 0) {
+        SOCKET = socket(AF_INET, SOCK_DGRAM, 0);
+        if (SOCKET < 0) {
+            perror("socket");
+            exit(EXIT_FAILURE);
+        }
+    }
+}
+
+//Function to send message to server and receive answer using TCP
+void tcp_client(const char* server_ip, int server_port) {
+    struct sockaddr_in server_addr;
 
     // Set server address
     memset(&server_addr, 0, sizeof(server_addr));
@@ -33,7 +78,7 @@ void tcp_client(const char* server_ip, int server_port) {
     }
 
     // Connect to the server
-    if (connect(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
+    if (connect(SOCKET, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
         perror("connect");
         exit(EXIT_FAILURE);
     }
@@ -42,33 +87,27 @@ void tcp_client(const char* server_ip, int server_port) {
     while(strcmp(buffer, "BYE\n") != 0) {
         bzero (buffer, BUF_SIZE);
         fgets(buffer, BUF_SIZE, stdin);
-        if (send(sockfd, buffer, strlen(buffer), 0) < 0) {
+        if (send(SOCKET, buffer, strlen(buffer), 0) < 0) {
             perror("send");
             exit(EXIT_FAILURE);
         }
 
         // Receive a response from the server
         memset(buffer, 0, sizeof(buffer));
-        if (recv(sockfd, buffer, sizeof(buffer), 0) < 0) {
+        if (recv(SOCKET, buffer, sizeof(buffer), 0) < 0) {
             perror("recv");
             exit(EXIT_FAILURE);
         }
 
-        printf("Server response: %s\n", buffer);
+        printf("%s\n", buffer);
     }
     // Close the socket
-    close(sockfd);
+    close(SOCKET);
 
 }
 
 //Function to send message to server and receive answer using UDP
 void udp_client(const char* server_ip, int server_port) {
-    // Create a UDP socket
-    int sock = socket(AF_INET, SOCK_DGRAM, 0);
-    if (sock < 0) {
-        perror("Failed to create socket");
-        exit(1);
-    }
 
     // Set up the server address
     struct sockaddr_in server_addr;
@@ -78,7 +117,6 @@ void udp_client(const char* server_ip, int server_port) {
     server_addr.sin_port = htons(server_port);
 
     // Prompt the user for a message
-    char buffer[BUF_SIZE];
     while (strcmp(buffer, "BYE\n") != 0) {
         bzero (buffer, BUF_SIZE);
         fgets(buffer, BUF_SIZE, stdin);
@@ -90,25 +128,25 @@ void udp_client(const char* server_ip, int server_port) {
         memcpy(new_buffer + 2, buffer, strlen(buffer));
 
         // Send the message to the server
-        int len = sendto(sock, new_buffer, strlen(buffer) + 2, 0, (struct sockaddr *) &server_addr, sizeof(server_addr));
+        int len = sendto(SOCKET, new_buffer, strlen(buffer) + 2, 0, (struct sockaddr *) &server_addr, sizeof(server_addr));
         if (len < 0) {
             perror("Failed to send message");
-            close(sock);
+            close(SOCKET);
             exit(1);
         }
 
         // Receive the server's response
         memset(buffer, 0, sizeof(buffer));
-        len = recvfrom(sock, buffer, BUF_SIZE, 0, NULL, NULL);
+        len = recvfrom(SOCKET, buffer, BUF_SIZE, 0, NULL, NULL);
         if (len < 0) {
             perror("Failed to receive response");
-            close(sock);
+            close(SOCKET);
             exit(1);
         }
 
         int status_code = (int) buffer[1];
         if(status_code == 0){
-            printf("Received response: %s\n", buffer + 2);
+            printf("%s\n", buffer + 2);
         } else {
             exit(1);
         }
@@ -141,11 +179,8 @@ int main(int argc, char *argv[]) {
         }
     }
 
-    //check if all arguments are entered
-    if (host == NULL || port == 0 || mode == NULL) {
-        fprintf(stderr, "Usage: %s -h host -p port -m mode\n", argv[0]);
-        exit(EXIT_FAILURE);
-    }
+    validate(host, port, mode);
+    create_socket(mode);
 
     //check if mode is tcp or udp
     if (strcmp(mode, "tcp") == 0){
